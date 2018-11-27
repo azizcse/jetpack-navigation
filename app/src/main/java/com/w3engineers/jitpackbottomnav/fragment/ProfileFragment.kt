809a0e0c02e225417d12a49151f8282dd8062f87
azizul <azizul@w3engineers.com>
2018-11-27 15:27:05 +0600

    rename the fragment

--- app/src/main/java/com/w3engineers/jitpackbottomnav/fragment/ProfileFragment.kt
+++ app/src/main/java/com/w3engineers/jitpackbottomnav/fragment/ProfileFragment.kt
@@ -1,16 +1,20 @@
 package com.w3engineers.jitpackbottomnav.fragment
 
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
 import android.widget.Toast
+import androidx.core.app.ShareCompat
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.w3engineers.jitpackbottomnav.R
-import kotlinx.android.synthetic.main.fragment_exanple.view.*
 
 
 /*
 *  ****************************************************************************
-*  * Created by : Md. Azizul Islam on 11/26/2018 at 4:33 PM.
+*  * Created by : Md. Azizul Islam on 11/26/2018 at 3:52 PM.
 *  * Email : azizul@w3engineers.com
 *  * 
 *  * Purpose:
@@ -21,8 +25,8 @@ import kotlinx.android.synthetic.main.fragment_exanple.view.*
 *  ****************************************************************************
 */
 
-class ExampleFragment2 : Fragment() {
-
+class ProfileFragment: Fragment(), View.OnClickListener {
+    lateinit var openButton : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,21 +34,39 @@ class ExampleFragment2 : Fragment() {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_example2, container, false)
+        val view = inflater.inflate(R.layout.fragment_exanple, container, false)
+
+        val name = arguments?.get("name") as String
+
+
+        openButton = view.findViewById(R.id.open_fragment_example2)
+        openButton.setOnClickListener(this)
+
+        Toast.makeText(activity, name, Toast.LENGTH_LONG).show()
+
         return view
+    }
+
+    override fun onClick(p0: View?) {
+        Navigation.findNavController(view!!).navigate(R.id.open_fragment_example_second)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater?.inflate(R.menu.menu_profile_img, menu)
+        inflater?.inflate(R.menu.menu_profile, menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         return when (item?.itemId) {
-            R.id.share_menu->{
-                Toast.makeText(activity,"Share menu", Toast.LENGTH_SHORT).show()
+            R.id.serch_menu -> {
+                Toast.makeText(activity,"Search menu", Toast.LENGTH_SHORT).show()
+                return true
+            }
+            R.id.send_menu->{
+                Toast.makeText(activity,"Send menu", Toast.LENGTH_SHORT).show()
                 return true
             } else -> super.onOptionsItemSelected(item)
         }
     }
+
 }
--- app/src/main/java/com/w3engineers/jitpackbottomnav/fragment/ProfileImageFragment.kt
+++ app/src/main/java/com/w3engineers/jitpackbottomnav/fragment/ProfileImageFragment.kt
@@ -1,20 +1,16 @@
 package com.w3engineers.jitpackbottomnav.fragment
 
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.view.*
-import android.widget.Button
 import android.widget.Toast
-import androidx.core.app.ShareCompat
 import androidx.fragment.app.Fragment
-import androidx.navigation.Navigation
 import com.w3engineers.jitpackbottomnav.R
+import kotlinx.android.synthetic.main.fragment_exanple.view.*
 
 
 /*
 *  ****************************************************************************
-*  * Created by : Md. Azizul Islam on 11/26/2018 at 3:52 PM.
+*  * Created by : Md. Azizul Islam on 11/26/2018 at 4:33 PM.
 *  * Email : azizul@w3engineers.com
 *  * 
 *  * Purpose:
@@ -25,8 +21,8 @@ import com.w3engineers.jitpackbottomnav.R
 *  ****************************************************************************
 */
 
-class ExampleFragment: Fragment(), View.OnClickListener {
-    lateinit var openButton : Button
+class ProfileImageFragment : Fragment() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,39 +30,21 @@ class ExampleFragment: Fragment(), View.OnClickListener {
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_exanple, container, false)
-
-        val name = arguments?.get("name") as String
-
-
-        openButton = view.findViewById(R.id.open_fragment_example2)
-        openButton.setOnClickListener(this)
-
-        Toast.makeText(activity, name, Toast.LENGTH_LONG).show()
-
+        val view = inflater.inflate(R.layout.fragment_example2, container, false)
         return view
-    }
-
-    override fun onClick(p0: View?) {
-        Navigation.findNavController(view!!).navigate(R.id.open_fragment_example_second)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
         super.onCreateOptionsMenu(menu, inflater)
-        inflater?.inflate(R.menu.menu_profile, menu)
+        inflater?.inflate(R.menu.menu_profile_img, menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         return when (item?.itemId) {
-            R.id.serch_menu -> {
-                Toast.makeText(activity,"Search menu", Toast.LENGTH_SHORT).show()
-                return true
-            }
-            R.id.send_menu->{
-                Toast.makeText(activity,"Send menu", Toast.LENGTH_SHORT).show()
+            R.id.share_menu->{
+                Toast.makeText(activity,"Share menu", Toast.LENGTH_SHORT).show()
                 return true
             } else -> super.onOptionsItemSelected(item)
         }
     }
-
 }
