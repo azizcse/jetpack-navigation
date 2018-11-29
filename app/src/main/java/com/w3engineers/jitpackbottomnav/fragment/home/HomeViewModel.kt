5151cde603cade473ca73346648e31efcfca1666
Aziz <imazizul@gmail.com>
2018-11-30 01:48:25 +0600

    Save in db

--- app/src/main/java/com/w3engineers/jitpackbottomnav/fragment/home/HomeViewModel.kt
+++ app/src/main/java/com/w3engineers/jitpackbottomnav/fragment/home/HomeViewModel.kt
@@ -5,18 +5,18 @@ import androidx.lifecycle.ViewModel
 import com.w3engineers.jitpackbottomnav.App
 import com.w3engineers.jitpackbottomnav.data.model.User
 import io.objectbox.Box
-import io.objectbox.BoxStore
 import io.objectbox.android.ObjectBoxLiveData
 import java.util.*
 
 class HomeViewModel : ViewModel() {
+
     private lateinit var userLiveData: ObjectBoxLiveData<User>
     val userBox: Box<User> = App.getBoxStore().boxFor(User::class.java)
+
     fun getUserLiveData(): ObjectBoxLiveData<User> {
          userLiveData = ObjectBoxLiveData<User>(userBox.query().build())
         return userLiveData
     }
-
 
     fun saveUser(name : String){
         Log.e("Navigation", "Save  called")
