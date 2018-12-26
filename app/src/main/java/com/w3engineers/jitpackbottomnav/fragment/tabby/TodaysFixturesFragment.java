c3f57ed8bd9dfda6f09d6b4ea67c547bac9edda7
Sudipta K Paik <sudipta@w3engineers.com>
2018-12-26 13:11:11 +0600

    Tabs Content added for Todays

--- app/src/main/java/com/w3engineers/jitpackbottomnav/fragment/tabby/TodaysFixturesFragment.java
+++ app/src/main/java/com/w3engineers/jitpackbottomnav/fragment/tabby/TodaysFixturesFragment.java
@@ -1,6 +1,35 @@
 package com.w3engineers.jitpackbottomnav.fragment.tabby;
 
+import android.util.Log;
+import android.view.View;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import com.core.kbasekit.ui.base.ItemClickListener;
+import com.w3engineers.jitpackbottomnav.R;
+import com.w3engineers.jitpackbottomnav.data.model.User;
+import com.w3engineers.jitpackbottomnav.databinding.FragmentTodaysFixtureBinding;
+import com.w3engineers.jitpackbottomnav.fragment.home.HomeViewModel;
+import com.w3engineers.jitpackbottomnav.fragment.home.UserPagedListAdapter;
+import org.jetbrains.annotations.NotNull;
+import org.workfort.base.ui.base.BaseFragment;
+
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.*;
+import android.widget.Toast;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
+import androidx.paging.PagedList;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import com.core.kbasekit.ui.base.ItemClickListener;
+import com.w3engineers.jitpackbottomnav.R;
+import com.w3engineers.jitpackbottomnav.data.model.User;
+import com.w3engineers.jitpackbottomnav.databinding.FragmentHomeBinding;
+import org.workfort.base.ui.base.BaseFragment;
 
 /**
  * * ============================================================================
@@ -24,5 +53,70 @@ import androidx.fragment.app.Fragment;
  * * ============================================================================
  **/
 
-public class TodaysFixturesFragment extends Fragment {
+public class TodaysFixturesFragment extends BaseFragment implements ItemClickListener<User> {
+
+    FragmentTodaysFixtureBinding binding;
+
+    HomeViewModel homeViewModel;
+
+    UserPagedListAdapter pagedAdapter;
+
+    @Override
+    public int getGetLayoutId() {
+        return R.layout.fragment_todays_fixture;
+    }
+
+    @Override
+    public int getGetMenuId() {
+        return 0;
+    }
+
+    @Override
+    public void startView() {
+
+        binding = (FragmentTodaysFixtureBinding) getViewBinding();
+
+        homeViewModel = getViewModel();
+        initRecyclerView();
+        loadData();
+    }
+
+    private void loadData() {
+        homeViewModel.getPagedUserLiveData().observe(this,
+                new Observer<PagedList<User>>() {
+                    @Override
+                    public void onChanged(PagedList<User> users) {
+                        Log.e("Item_list", "User List size =" + users.size());
+                        pagedAdapter.submitList(users);
+                    }
+                });
+    }
+
+    private void initRecyclerView() {
+        pagedAdapter = new UserPagedListAdapter(getContext(), this);
+        binding.recyclerViewAllFixtures.setAdapter(pagedAdapter);
+        binding.recyclerViewAllFixtures.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.recyclerViewAllFixtures.setHasFixedSize(true);
+        // binding.openProfilePage.setOnClickListener(this)
+    }
+
+    private HomeViewModel getViewModel() {
+        return ViewModelProviders.of(this).get(HomeViewModel.class);
+    }
+
+    @Override
+    public void stopView() {
+
+    }
+
+    @NotNull
+    @Override
+    public Fragment currentFragment() {
+        return this;
+    }
+
+    @Override
+    public void onItemClick(@NotNull View view, User item) {
+
+    }
 }
